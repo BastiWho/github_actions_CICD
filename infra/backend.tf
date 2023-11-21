0e5b315f56574425a1f1f8d6c36cdf121c47b8fc
Basti <sebastian.hufeld@docc.techstarter.de>
2023-11-21 15:03:34 +0100

    change backend.tf File

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,11 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
   backend "s3" {
     bucket = "{{ secrets.TFSTATE_BUCKET_NAME }}"
     key    = "{{ secrets.TFSTATE_KEY }}"
