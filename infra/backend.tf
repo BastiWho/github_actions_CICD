890531a0e11c01cf1dffc09732f84f9c7361e810
Basti <sebastian.hufeld@docc.techstarter.de>
2023-11-21 15:32:36 +0100

    backend http

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,10 @@
 terraform {
-  backend "s3" {
-    bucket = "{{ secrets.TFSTATE_BUCKET_NAME }}"
-    key    = "{{ secrets.TFSTATE_KEY }}"
-    region = "eu-central-1"
+  backend "http" {
+    address        = "https://api.tfstate.dev/github/v1"
+    lock_address   = "https://api.tfstate.dev/github/v1/lock"
+    unlock_address = "https://api.tfstate.dev/github/v1/lock"
+    lock_method    = "PUT"
+    unlock_method  = "DELETE"
+    username       = "BastiWho/github_actions_CICD"
   }
 }
