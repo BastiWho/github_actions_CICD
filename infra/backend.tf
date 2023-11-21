35aa2a2d8468f44fc693ba924f64adf69fcfda88
Basti <sebastian.hufeld@docc.techstarter.de>
2023-11-21 15:11:19 +0100

    region geändert, backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "{{ secrets.TFSTATE_BUCKET_NAME }}"
     key    = "{{ secrets.TFSTATE_KEY }}"
-    region = var.region
+    region = var.aws_region
   }
 }
